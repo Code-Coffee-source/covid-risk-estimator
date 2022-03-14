34536dc1fb4c0681f066f5579bedd98e1caa4af8
Code-Coffee-source <coffee.code693@gmail.com>
2022-03-14 19:48:36 +0800

    added cusom settings to room-size

--- static/js/room-size.js
+++ static/js/room-size.js
@@ -4,22 +4,47 @@
     var inputFields = $('#room_width, #room_height, #room_length');
     var sizeBtns = $('.sizeBtn');
     var roomSizeAnswer = getAnswer('room-size')
+    var roomWidthAnswer = getAnswer('room-width');
+    var roomHeightAnswer = getAnswer('room-height');
+    var roomLengthAnswer = getAnswer('room-length');
+
+    function checkInputs() {
+        var isValid = true;
+
+        inputFields.each(function() {
+            if ($(this).val().length == 0) isValid = false;
+        });
+
+        return isValid;
+    };
 
     function checkIfDone() {
         if (getAnswer('room-width') && getAnswer('room-height') && getAnswer('room-length')) return true;
-    }
+    };
 
     function makeActive(id) {
 
         $('#'+id).addClass('bg-dirty-white').removeClass('bg-beige');
         $('#'+id).siblings().addClass('bg-dirty-white').removeClass('bg-beige');
 
-    }
+    };
+
+    inputFields.each(function() {
+        $(this).change(function(event) {
+            var keyForStorage = 'room-' + this.id.split('_')[1]; // room-width, room-height, or room-length;
+            storeAnswer(keyForStorage, this.value);
+            if (checkInputs()) allowNextButton();
+        }.bind(this));
+    });
 
     sizeBtns.each(function() {
         $(this).click(function(event) {
 
             var item = $(this);
+
+            $('#room_width').val(item.attr('data-width'));
+            $('#room_height').val(item.attr('data-height'));
+            $('#room_length').val(item.attr('data-length'));
 
             storeAnswer('room-width', parseInt(item.attr('data-width')));
             storeAnswer('room-height', parseInt(item.attr('data-height')));
@@ -34,6 +59,12 @@
         }.bind(this));
     });
 
-    if (roomSizeAnswer) makeActive(roomSizeAnswer)
+    if (roomWidthAnswer) $('#room_width').val(roomWidthAnswer);
+    if (roomHeightAnswer) $('#room_height').val(roomHeightAnswer);
+    if (roomLengthAnswer) $('#room_length').val(roomLengthAnswer);
+
+
+    if (roomSizeAnswer) makeActive(roomSizeAnswer);
+    if (checkInputs()) allowNextButton();
 
 })();
