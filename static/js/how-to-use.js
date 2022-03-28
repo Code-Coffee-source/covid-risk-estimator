880f360038edca40ce9746f09a0d728e2e784c16
Code-Coffee-source <coffee.code693@gmail.com>
2022-03-28 13:33:30 +0800

    updated carousels and how-to-use page

--- static/js/how-to-use.js
+++ static/js/how-to-use.js
@@ -1,65 +1,36 @@
 (function() {
 
-    var extraBtn = $('#extraDivBtn');
-    var extraDiv = $('#extraDiv');
     var nextBtn = $('#next-btn');
     var backBtn = $('#back-btn');
     var startBtn = $('#start-btn');
     var endBtn = $('#end-btn');
     var numBtn = $('#num-btn');
 
-    extraBtn.click(function() {
-
-    extraDiv.toggleClass('closed open')
-    extraDiv.toggleClass('h-0 h-full')
-    extraDiv.children().toggleClass('h-0 h-full')
-
-    });
-
     nextBtn.on('click', function() {
-        var currNum = parseInt($(this).attr('href').split("_")[1])
+        var nextNum = parseInt($(this).attr('href').split("_")[1])
+        var backNum = parseInt(backBtn.attr('href').split("_")[1])
 
-        if (!(currNum == parseInt($('.carousel-item').length)+1)) {
-            nextBtn.attr('href', '#item_'+(currNum+1));
-            backBtn.attr('href', '#item_'+(currNum-1));
-            numBtn.html(currNum);
+        if (!(nextNum == parseInt($('.carousel-item').length))) {
 
-        };
+            nextBtn.attr('href', '#slide_'+(nextNum+1));
+            backBtn.attr('href', '#slide_'+(backNum+1));
 
-        if (!(currNum == parseInt($('.carousel-item').length)+1)) {
+            numBtn.html(nextNum)
 
         };
 
     });
 
     backBtn.on('click', function() {
-        var currNum = parseInt($(this).attr('href').split("_")[1])
+        var nextNum = parseInt(nextBtn.attr('href').split("_")[1])
+        var backNum = parseInt($(this).attr('href').split("_")[1])
 
-        if (!(currNum == 0)) {
-            nextBtn.attr('href', '#item_'+(currNum+1))
-            backBtn.attr('href', '#item_'+(currNum-1))
-            numBtn.html(currNum);
+        if (!(backNum == -1)) {
+            nextBtn.attr('href', '#slide_'+(nextNum-1));
+            backBtn.attr('href', '#slide_'+(backNum-1));
+            numBtn.html(backNum)
+
         };
-
-    });
-
-    endBtn.on('click', function() {
-        var endNum = parseInt($(this).attr('href').split('_')[1]);
-
-        numBtn.html(endNum);
-
-        nextBtn.attr('href', '#item_'+(endNum+1));
-        backBtn.attr('href', '#item_'+(endNum-1));
-
-    });
-
-    startBtn.on('click', function() {
-        var startNum = parseInt($(this).attr('href').split('_')[1]);
-
-        numBtn.html(startNum);
-
-        nextBtn.attr('href', '#item_'+(startNum+1));
-        backBtn.attr('href', '#item_'+(startNum-1));
 
     });
 
